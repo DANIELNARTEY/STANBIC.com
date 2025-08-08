c39586263d6812ded8d013d47dc95a8b3d97182d
DANIELNARTEY <danielnartey2004@gmail.com>
2025-08-08 18:39:51 +0000

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,7 @@
+function addToCart(name, price) {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push({ name, price });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added to cart!");
+  }
+  
